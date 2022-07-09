ce517f9c73c8ad596e4cb99bb4ccf2389b9cb929
bassem eloraby <BASEM12683@GMAIL.COM>
2022-07-09 11:28:52 +0300

    show hide

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,20 +2,33 @@ import React, { useState } from 'react';
 import './App.css';
 import Card from '../Card/Card';
 const App = () => {
+  const [cardToggle, setCardToggle] = useState(false);
   const [state, setState] = useState([
     { name: 'bassem', age: 38, adress: 'madina', phone: '01013456994' },
     { name: 'mohammed', age: 30, adress: 'alex', phone: '054555224777' },
     { name: 'ahmed', age: 20, adress: 'jada', phone: '05688742554' },
   ]);
 
-  const deleteHndeler = (e,clickedIdx) => {
-    const deleteOperation = state.filter((el,idx) => idx !== clickedIdx)
-    setState(deleteOperation)
+  const deleteHndeler = (e, clickedIdx) => {
+    // const deleteOperation = state.filter((el,idx) => idx !== clickedIdx)
+    // setState(deleteOperation)
+    setState((prvState) => {
+      return prvState.filter((el, idx) => idx !== clickedIdx);
+    });
+  };
+
+  const toggleHandelr = () =>{
+    setCardToggle(!cardToggle)
   }
   return (
     <div className="mainContainer">
-      <h2>boys namesList</h2>
-      <Card namesList={state} type="men" deletefunc = {deleteHndeler} />
+      <h1>boys namesList</h1>
+      <button style={{ marginBottom: '20px' }} onClick ={toggleHandelr}>
+        {cardToggle ? 'hide names' : 'show names'}
+      </button>
+      <div className={cardToggle ? 'show' : 'hide'}>
+        <Card namesList={state} type="men" deletefunc={deleteHndeler} />
+      </div>
     </div>
   );
 };
