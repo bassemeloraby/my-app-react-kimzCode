d5d366de4a6d61638db7279891e016d10b86baba
bassem eloraby <BASEM12683@GMAIL.COM>
2022-07-13 13:38:50 +0300

    React Hooks - Modal Pop up with Fragments - Part 2

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,10 +1,25 @@
 import React,{Fragment} from 'react';
 import styles from './Modal.module.css';
+import ReactDOM from 'react-dom';
+
+const BackDrop = () => {
+  return  <div className={styles.backDrop}></div>
+};
+
+ const Overlay=() => {
+  return   <div className={styles.overlay}></div>
+};
+
 const Modal = () => {
   return <Fragment>
-  <div className={styles.backDrop}></div>
-  <div className={styles.overlay}></div>
-  </Fragment>;
+  {ReactDOM.createPortal(
+    <Fragment>
+      <BackDrop/>
+      <Overlay/>
+    </Fragment>,
+    document.getElementById('Modal')
+  )}
+  </Fragment>
 };
 
 export default Modal;
